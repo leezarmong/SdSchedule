d9ea319f9f684132fe8fc3348c37cf3b8ec0ff8e
leezarmong <leezarmong7@gmail.com>
2024-05-01 00:00:32 +0900

    StringBoot

--- Project/SdProject/src/main/java/com/sd/schedule/SdProjectApplication.java
+++ Project/SdProject/src/main/java/com/sd/schedule/SdProjectApplication.java
@@ -3,6 +3,8 @@ package com.sd.schedule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//어플리케이션
 @SpringBootApplication
 public class SdProjectApplication {
 
