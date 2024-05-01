379daa200f24d72fc5f862499a796a518ffbbd70
leezarmong <leezarmong7@gmail.com>
2024-05-01 19:47:51 +0900

    selete

--- DB/SDSC.sql
+++ DB/SDSC.sql
@@ -5,3 +5,11 @@ CREATE TABLE member (
 insert into member(member_name) values('유건희');
 
 select * from member;
+
+select member_name from member;
+
+
+select member_name from member;
+
+
+insert into member values('원동하');
