ea7f35796c2a76e24ea277a4c60a70b3e9bd1c2b
makePuzzle <createve1436@gmail.com>
2024-05-03 17:12:19 +0900

    2024-05-03-1712

--- FrontEnd/js/schedule.js
+++ FrontEnd/js/schedule.js
@@ -4,7 +4,6 @@ import {
 } from "./util.js";
 
 const Excel = document.getElementById("excel-upload");
-const TextArea = document.getElementById("json_data");
 const wt_table_head = document.querySelector("thead");
 const wt_table_body = document.querySelector("tbody");
 const excel_upload_btn = document.querySelector("label");
@@ -16,37 +15,37 @@ tdElements.forEach(td=>{
     memberList.push(memberName);
 });
 Excel.onchange = function () {
-    // const memberList = [
-    //     "김장현",
-    //     "김해수",
-    //     "최인화",
-    //     "유건희",
-    //     "이희정",
-    //     "강민지",
-    //     "권태영",
-    //     "김경민",
-    //     "김무준",
-    //     "김세희",
-    //     "김영록",
-    //     "김은경",
-    //     "김지환",
-    //     "박대용",
-    //     "박현선",
-    //     "복금현",
-    //     "서준영",
-    //     "안지연",
-    //     "원동하",
-    //     "유영현",
-    //     "윤승관",
-    //     "윤승관",
-    //     "이상건",
-    //     "이영현",
-    //     "이재원",
-    //     "전예준",
-    //     "조경서",
-    //     "조관우",
-    //     "홍지오"
-    // ];
+    const memberList = [
+        "김장현",
+        "김해수",
+        "최인화",
+        "유건희",
+        "이희정",
+        "강민지",
+        "권태영",
+        "김경민",
+        "김무준",
+        "김세희",
+        "김영록",
+        "김은경",
+        "김지환",
+        "박대용",
+        "박현선",
+        "복금현",
+        "서준영",
+        "안지연",
+        "원동하",
+        "유영현",
+        "윤승관",
+        "윤승관",
+        "이상건",
+        "이영현",
+        "이재원",
+        "전예준",
+        "조경서",
+        "조관우",
+        "홍지오"
+    ];
     readXlsxFile(Excel.files[0]).then(data=>{
         // code for work table head
         let wt_table_head_html = `
@@ -117,6 +116,11 @@ Excel.onchange = function () {
         });
         // Insert converted Json to table
         let wt_table_body_html = "";
+        const total = {
+            weekWorkTime: 0,
+            nightWorkTime: 0,
+            weekPay: 0
+        };
         for(let w = 0; w < sortedKeys.length; w++){
             // get weekly work time, night work time
             let weekWorkTime = 0;
@@ -138,11 +142,14 @@ Excel.onchange = function () {
             };
             // get isOver, weekPay
             let isOver = 'O';
+            let classPay = 9875;
             let weekPay = 0;
             if(weekWorkTime>=15){
                 isOver = 'O';
+                weekPay = Math.round((weekWorkTime * 1.2 + nightWorkTime * 0.5) * classPay);
             }else{
                 isOver = 'X';
+                weekPay = Math.round((weekWorkTime + nightWorkTime * 0.5) * classPay);
             };
             wt_table_body_html += `
                 <tr>
@@ -152,10 +159,24 @@ Excel.onchange = function () {
                     <td>${weekWorkTime} h</td>
                     <td>${nightWorkTime} h</td>
                     <td>${isOver}</td>
-                    <td>${'아직 급여 계산 방식을 이해 못했음'} ₩</td>
+                    <td>${numberWithCommas(weekPay)} ₩</td>
                 </tr>
             `;
+            total.weekWorkTime += weekWorkTime;
+            total.nightWorkTime += nightWorkTime;
+            total.weekPay += weekPay;
         };
+        wt_table_body_html = `
+            <tr class="bold">
+                <td>합계</td>
+                <td></td>
+                <td></td>
+                <td>${total.weekWorkTime} h</td>
+                <td>${total.nightWorkTime} h</td>
+                <td></td>
+                <td>${numberWithCommas(total.weekPay)} ₩</td>
+            </tr>
+        ` + wt_table_body_html;
         wt_table_body.innerHTML = wt_table_body_html;
     });
     excel_upload_btn.style.display = "none";
