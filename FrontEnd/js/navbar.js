d11a948c71cbb7eaef0102f50ec923a81a20c25e
makePuzzle <createve1436@gmail.com>
2024-05-31 15:38:26 +0900

    서순 뒤집기

--- FrontEnd/js/navbar.js
+++ FrontEnd/js/navbar.js
@@ -3,8 +3,15 @@
 const navbar = document.getElementById("navbar");
 
 let navbar_html = `
-    <a href="/">메인</a>
-    <a href="/memberpage">멤버</a>
+    <i id="openNav" class="fa-solid fa-bars fa-lg"></i>
+    <div id="hiddenMenu">
+        <div id="navHeader">
+            <i id="closeNav" class="fa-solid fa-bars fa-lg"></i>
+            <img src="../img/logo.png" />
+        </div>
+        <a href="/">메인</a>
+        <a href="/memberpage">멤버</a>
+    </div>
 `;
 
 navbar.innerHTML = navbar_html;
