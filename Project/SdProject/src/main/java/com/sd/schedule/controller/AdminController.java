cdb2cf697b02805bea92fe61011f95bd141bf566
leezarmong <leezarmong7@gmail.com>
2024-09-09 15:37:40 +0900

    memberstationCSS

--- Project/SdProject/src/main/java/com/sd/schedule/controller/AdminController.java
+++ Project/SdProject/src/main/java/com/sd/schedule/controller/AdminController.java
@@ -21,20 +21,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 
-
-
 @Controller
 public class AdminController {
-	
-	@Autowired
-	private AdminService adminService;
-	
 
-	
-	
-	
-	
-	//admin 페이지
+    @Autowired
+    private AdminService adminService;
+
+
+    //admin 페이지
 //	@GetMapping("/adminpage")
 //	 public String adminPage(@RequestParam(defaultValue = "1") int curPage, MemberVO vo, Model model, HttpSession session, HttpServletRequest request) {
 //			
@@ -63,53 +57,50 @@ public class AdminController {
 //	        
 //	        return "admin/adminpage";
 //	    }
-	
-	
-	
-	//admin search page
-	@GetMapping("/adminSearchMember")
-	public String adminsearchmember(Model model, HttpSession session, String name, @RequestParam(defaultValue = "1") int curPage) {
-		
-		
-	
-		int count = adminService.adminCountSearch(name);
+
+
+    //admin search page
+    @GetMapping("/adminSearchMember")
+    public String adminsearchmember(Model model, HttpSession session, String name, @RequestParam(defaultValue = "1") int curPage) {
+
+
+        int count = adminService.adminCountSearch(name);
 //		System.out.print(count);
-	       
-	        Pager pager = new Pager(count, curPage);
-	        int start = pager.getPageBegin();
-	        int end = pager.getPageEnd();
-	        
-	        session.setAttribute("name",name);	// 아이디 검색
-	        session.setAttribute("curPage", curPage);
-	        
-	      
-	        
-	        List<MemberVO> list = adminService.adminSearchMember(name, start, end);
+
+        Pager pager = new Pager(count, curPage);
+        int start = pager.getPageBegin();
+        int end = pager.getPageEnd();
+
+        session.setAttribute("name", name);    // 아이디 검색
+        session.setAttribute("curPage", curPage);
+
+
+        List<MemberVO> list = adminService.adminSearchMember(name, start, end);
 //	        System.out.print(user_id);
-	        HashMap<String, Object> map = new HashMap<String, Object>();
-	        map.put("list", list);
-	        map.put("count", count);
-	        map.put("pager", pager);
-	        map.put("name", name);
-	        model.addAttribute("map", map);
-	        
-	        return "admin/adminsearchmember";
-	    }
-	
-	//관리자 전용 멤버 업데이트
-	@GetMapping("/adminUpdateMember")
-	public String adminUpdateMember (MemberVO vo) {
-		
-		adminService.adminUpdateMember(vo);
-		return  "redirect:adminpage";
-	}
-	
-	//멤버 추가
-	@PostMapping("/adminInsertMember")
-	public String adminInsertMember(MemberVO vo){
-		
-		adminService.adminInsertMember(vo);
-		return "redirect:adminpage";
-	}
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("list", list);
+        map.put("count", count);
+        map.put("pager", pager);
+        map.put("name", name);
+        model.addAttribute("map", map);
+
+        return "admin/adminsearchmember";
+    }
+
+    //관리자 전용 멤버 업데이트
+    @GetMapping("/adminUpdateMember")
+    public String adminUpdateMember(MemberVO vo) {
+
+        adminService.adminUpdateMember(vo);
+        return "redirect:adminpage";
+    }
+
+    //멤버 추가
+    @PostMapping("/adminInsertMember")
+    public String adminInsertMember(MemberVO vo) {
+
+        adminService.adminInsertMember(vo);
+        return "redirect:adminpage";
+    }
 
 }
--- Project/SdProject/src/main/java/com/sd/schedule/model/station/StationService.java
+++ Project/SdProject/src/main/java/com/sd/schedule/model/station/StationService.java
@@ -7,27 +7,25 @@ import org.apache.ibatis.annotations.Param;
 import com.sd.schedule.model.member.MemberVO;
 
 
-
-
 public interface StationService {
-	
-	
-	// 스테이션 리스트
+
+
+    // 스테이션 리스트
 //	public List<StationVO> stationListByUserId(String user_id);
-	
-	//스테이션 입력
-	public void insertStation (StationVO svo);
-	
-	//스테이션 삭제
-	public void deleteStation (String member_name);
-	
-	//스테이션 업데이트
-	public void updateStation (StationVO svo);
-	
-	//스테이션 이름 추출
-	public List<StationVO> selectStationsByMemberNames(List<String> memberNames);
-	
-	//불필요 멤버 전체 삭제
-	public void deleteStations(@Param("list") List<MemberVO> members);
+
+    //스테이션 입력
+    public void insertStation(StationVO svo);
+
+    //스테이션 삭제
+    public void deleteStation(String member_name);
+
+    //스테이션 업데이트
+    public void updateStation(StationVO svo);
+
+    //스테이션 이름 추출
+    public List<StationVO> selectStationsByMemberNames(List<String> memberNames);
+
+    //불필요 멤버 전체 삭제
+    public void deleteStations(@Param("list") List<MemberVO> members);
 
 }
